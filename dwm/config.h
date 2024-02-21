75d08e77e1a7a1960784d6a9928eac2919a37574
Kouei <hungjui11@gmail.com>
2024-02-21 09:08:00 +0800

    color fixed

--- dwm/config.h
+++ dwm/config.h
@@ -1,6 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
-#include "/home/agentk/.suckless/dwm/themes/darkblue.h" /* Importing Theme */
+static const char col_gray1[]       = "#16161e";
+static const char col_gray2[]       = "#b79059";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#16161e";
+static const char col_gray5[]       = "#ca6278";
+static const char col_cyan[]        = "#68aad2";
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
