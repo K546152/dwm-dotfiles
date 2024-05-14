8bf81713a1dd0285b016b5f166e657b38a5f3d03
Kouei <hungjui11@gmail.com>
2024-05-14 11:09:00 +0800

    vpn

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -8,11 +8,15 @@ if [ -n "$vpn_status" ]; then
   vpn_info=""
   if echo "$vpn_status" | grep -q 'vpn'; then
     # OpenVPN is active
-    vpn_info=$(echo "${vpn_info}OV")
+    vpn_info="${vpn_info}OV"
   fi
   if echo "$vpn_status" | grep -q 'wireguard'; then
     # WireGuard is active
-    vpn_info=$(echo "${vpn_info}WG")
+    if [ -n "$vpn_info" ]; then
+      vpn_info="${vpn_info}/WG"  # If both VPNs are active
+    else
+      vpn_info="${vpn_info}WG"    # If only WireGuard is active
+    fi
   fi
   echo "VPN:$vpn_info"  # You can customize this symbol to match your dwmblocks icons
 else
